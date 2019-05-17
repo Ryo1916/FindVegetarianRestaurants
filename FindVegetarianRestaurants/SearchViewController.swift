3c174c2718e71a1ae6d6ea3836c1d05529f52a9c
Ryo1916 <telemon1916@gmail.com>
2019-05-17 17:58:32 +0900

    Get the data from gurunavi API with free words

--- FindVegetarianRestaurants/SearchViewController.swift
+++ FindVegetarianRestaurants/SearchViewController.swift
@@ -17,17 +17,40 @@ class SearchViewController: UIViewController {
     // constants
     private let GURUNAVI_API = "https://api.gnavi.co.jp/ForeignRestSearchAPI/v3/"
     private let API_KEY = FindVegetarianRestaurantsKeys().gurunaviAPIKey
-    
+
+    typealias APIParams = [String: Any]
 
     @IBOutlet weak var appName: UILabel!
     @IBOutlet weak var searchField: ErrorTextField!
     @IBAction func searchByTextPressed(_ sender: UIButton) {
+        let freeWords = searchField.text!
+        getWeatherDataByFreeWords(url: GURUNAVI_API, freeWords: freeWords)
     }
     @IBAction func searchByLocationPressed(_ sender: UIButton) {
     }
+
+    // TODO: Create options selection "english_speaking_staff" and "english_menu"
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func getWeatherDataByFreeWords(url: String, freeWords: String) {
+        let paramsWithFreeWords: APIParams = ["keyid": API_KEY, "lang": "en", "freeword": freeWords, "english_speaking_staff": 0, "english_menu": 0, "vegetarian_menu_options": 1]
+
+        Alamofire.request(url, method: .get, parameters: paramsWithFreeWords).responseJSON { response in
+            if response.result.isSuccess {
+                print("Success! Got the weather data by free words.")
+                
+                let responseJSON: JSON = JSON(response.result.value!)
+                print(responseJSON)
+            } else {
+                print("Error \(String(describing: response.result.error))")
+                self.appName.text = "Connection Issues"
+                self.appName.textColor = .red
+            }
+        }
     }
 }
