5506eefa569f39646f4f7ab409cca05153c561e6
Ryo1916 <telemon1916@gmail.com>
2019-05-20 17:20:10 +0900

    Create restaurant entity

---
+++ FindVegetarianRestaurants/Restaurant.swift
@@ -0,0 +1,9 @@
+//
+//  Restaurant.swift
+//  FindVegetarianRestaurants
+//
+//  Created by Ryo Fujimoto on 2019/05/17.
+//  Copyright © 2019 Ryo Fujimoto. All rights reserved.
+//
+
+import Foundation
