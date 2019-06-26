5dc66565ac3008e97738f3d440494557a57cb2bb
Ryo1916 <telemon1916@gmail.com>
2019-06-26 17:16:15 +0900

    Create ResultsTableViewCell

---
+++ FindVegetarianRestaurants/ResultsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResultsTableViewCell.swift
+//  FindVegetarianRestaurants
+//
+//  Created by Ryo Fujimoto on 2019/05/21.
+//  Copyright © 2019 Ryo Fujimoto. All rights reserved.
+//
+
+import UIKit
+
+class ResultsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
